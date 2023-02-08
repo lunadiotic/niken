abacb46952b6535873610b3bc9ab500ee56a7b2c
aim <ahmadhakim404@gmail.com>
2023-02-08 23:15:40 +0700

    fix title change

--- hello/app.js
+++ hello/app.js
@@ -4,8 +4,6 @@ let hour = d.getHours();
 // Pagi
 if (hour < 12 && hour >= 4) {
 	document.querySelector('[icon="cloudy"]').style.display = 'block';
-	document.querySelector('title').innerHTML =
-		'Pagi Niken, Jangan Lupa Sarapan sama Mandi :p';
 	document.querySelector('body').style.backgroundColor = '#85C8C6';
 	document.querySelector('#greeting').style.color = 'dimgray';
 	let greetings = [
@@ -24,7 +22,6 @@ if (hour < 12 && hour >= 4) {
 // Siang
 if (hour < 15 && hour >= 12) {
 	document.querySelector('[icon="sunny"]').style.display = 'block';
-	document.querySelector('title').innerHTML = 'Siang Niken, Jangan Lupa Makan';
 	document.querySelector('body').style.backgroundColor = '#B6EEF5';
 	document.querySelector('#greeting').style.color = 'dimgray';
 	let greetings = [
@@ -43,8 +40,6 @@ if (hour < 15 && hour >= 12) {
 // Sore
 if (hour < 18 && hour >= 15) {
 	document.querySelector('[icon="afternoon"]').style.display = 'block';
-	document.querySelector('title').innerHTML =
-		'Sore, Nanti hati-hati pulangnya ya!';
 	document.querySelector('body').style.backgroundColor = '#85C8C6';
 	document.querySelector('#greeting').style.color = 'dimgray';
 	let greetings = [
@@ -61,7 +56,6 @@ if (hour < 18 && hour >= 15) {
 // Petang
 if (hour < 21 && hour >= 18) {
 	document.querySelector('[icon="snowy"]').style.display = 'block';
-	document.querySelector('title').innerHTML = "Niken, how's your day?";
 	document.querySelector('body').style.backgroundColor = '#48555A';
 	let greetings = [
 		'<p>...</p>^2000<p>Suka ngeteh atau cuma duduk diteras buat nyantai gak?</p>',
@@ -79,8 +73,6 @@ if (hour < 21 && hour >= 18) {
 // Bobo
 if (hour >= 21 || hour < 4) {
 	document.querySelector('[icon="supermoon"]').style.display = 'block';
-	document.querySelector('title').innerHTML =
-		"Already sleepy? For sure it's Bobo time";
 	document.querySelector('body').style.backgroundColor = '#1f2223';
 	let greetings = [
 		'<p>Terima kasih atas kerja kerasnya hari ini, Niken.</p>',
